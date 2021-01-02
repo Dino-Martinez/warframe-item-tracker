df84dec28ddd5f38c29bdc8ee786615d8441356c
Secundino Martinez <Dino.Martinez.514@gmail.com>
2021-01-02 14:02:46 -0800

    routed app.js

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import { HashRouter as Router, Route } from 'react-router-dom';
 import Footer from './Footer';
 import Header from './Header';
 import SearchPage from './SearchPage';
+import WatchList from './WatchList';
+import ItemInfo from './ItemInfo';
 
 function App() {
   return (
     <Router>
       <Header />
       <Route exact path="/" component={SearchPage} />
+      <Route path="/watchlist" component={WatchList}/>
+      <Route path="/items/:itemId" component={ItemInfo}/>
       <Footer />
     </Router>
   );
