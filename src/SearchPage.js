545c1e9fbed32c4997c7e0029527b7763e65a8e4
Tristan <tristan15thompson@gmail.com>
2021-01-02 16:11:41 -0800

    [EDIT] IN PROGRESS Search Page

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import './SearchPage.css';
+import {Link} from 'react-router-dom';
 
 class SearchPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      query: ""
+    };
+    this.updateQuery = this.updateQuery.bind(this);
+  }
+  updateQuery(event) {
+    this.setState({
+      query: event.target.value
+    });
+  }
   render() {
-    return <h1>Nothing yet...</h1>;
+    return(
+      <div className="jumbotron form-group">
+        <input type="text"  className="form-control" value={this.state.query} onChange={this.updateQuery} />
+        <Link to ={"/items/"+this.state.query} >search</Link>
+      </div>
+
+
+    );
   }
 }
 
