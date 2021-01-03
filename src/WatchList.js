7d2a3b378e5480d37e6150fb15ce4297de5cac6a
Secundino Martinez <Dino.Martinez.514@gmail.com>
2021-01-02 16:52:09 -0800

    added watchlist without styling

--- src/WatchList.js
+++ src/WatchList.js
@@ -2,8 +2,29 @@ import React from 'react';
 import './WatchList.css';
 
 class WatchList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      items: [
+        {id: "itemIdOne", name: "Test name", price: 10},
+        {id: "itemIdTwo", name: "Test name", price: 20},
+        {id: "itemIdThree", name: "Test name", price: 30},
+      ],
+      urgentItems: []
+    };
+  }
   render() {
-    return <h1>Nothing yet...</h1>;
+    return (
+      <div>
+        <ul>
+          {
+            this.state.items.map((item) => {
+              return <li key={item.id}>{item.name} | {item.price}</li>
+            })
+          }
+        </ul>
+      </div>
+    );
   }
 }
 
