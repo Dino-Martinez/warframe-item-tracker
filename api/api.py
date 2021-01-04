6bea2aa9d09a47ee401a2c6ae7a59e9b4d52b2b1
Secundino Martinez <Dino.Martinez.514@gmail.com>
2021-01-04 00:01:07 -0800

    backend api running with mock data, displayed on watchlist

--- api/api.py
+++ api/api.py
@@ -1,7 +1,17 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
 @app.route('/api/')
 def api_home():
-    return {"data": "Hello, World!"}
+    r = random.randint(1, 100)
+    items = [
+        {"id": "itemIdOne", "name": "Test data", "average": r, "min": 5, "max": 15, "isUrgent": False},
+        {"id": "itemIdTwo", "name": "Test data", "average": r, "min": 15, "max": 25, "isUrgent": False},
+        {"id": "itemIdThree", "name": "Test data", "average": r, "min": 35, "max": 35, "isUrgent": False},
+        {"id": "itemIdFour", "name": "Test data", "average": r, "min": 35, "max": 35, "isUrgent": True},
+        {"id": "itemIdFive", "name": "Test data", "average": r, "min": 35, "max": 35, "isUrgent": False},
+        {"id": "itemIdSix", "name": "Test data", "average": r, "min": 35, "max": 35, "isUrgent": True},
+    ]
+    return {"items": items }
--- src/WatchList.js
+++ src/WatchList.js
@@ -6,21 +6,14 @@ class WatchList extends React.Component {
   constructor() {
     super();
     this.state = {
-      items: [
-        {id: "itemIdOne", name: "Test name", average: 10, min: 5, max: 15, isUrgent: false},
-        {id: "itemIdTwo", name: "Test name", average: 20, min: 15, max: 25, isUrgent: false},
-        {id: "itemIdThree", name: "Test name", average: 30, min: 35, max: 35, isUrgent: false},
-        {id: "itemIdFour", name: "Test name", average: 30, min: 35, max: 35, isUrgent: true},
-        {id: "itemIdFive", name: "Test name", average: 30, min: 35, max: 35, isUrgent: false},
-        {id: "itemIdSix", name: "Test name", average: 30, min: 35, max: 35, isUrgent: true},
-      ],
-      result: '',
+      items: [],
     };
 
     this.updateData = this.updateData.bind(this);
   }
 
   componentDidMount() {
+    this.updateData();
     this.intervalID = setInterval(this.updateData, 2000);
   }
 
@@ -31,25 +24,16 @@ class WatchList extends React.Component {
 
   // Check periodically for api update, set state to new data
   async updateData() {
-    console.log('updating...');
-    const items = [
-      {id: "itemIdOne", name: "Test name", average: 10, min: Math.floor((Math.random() * 100) + 1), max: 15, isUrgent: false},
-      {id: "itemIdTwo", name: "Test name", average: 20, min: Math.floor((Math.random() * 100) + 1), max: 25, isUrgent: false},
-      {id: "itemIdThree", name: "Test name", average: 30, min: Math.floor((Math.random() * 100) + 1), max: 35, isUrgent: false},
-      {id: "itemIdFour", name: "Test name", average: 30, min: Math.floor((Math.random() * 100) + 1), max: 35, isUrgent: true},
-      {id: "itemIdFive", name: "Test name", average: 30, min: Math.floor((Math.random() * 100) + 1), max: 35, isUrgent: false},
-      {id: "itemIdSix", name: "Test name", average: 30, min: Math.floor((Math.random() * 100) + 1), max: 35, isUrgent: true},
-    ];
     const result = await fetch('/api/');
     const json = await result.json();
     console.log(json);
-    this.setState({ items, result: json });
+    this.setState({ items: json.items });
   }
 
   render() {
     return (
       <div className="container text-center mt-5">
-        <h2>Watched Items:{this.state.result.data}</h2>
+        <h2>Watched Items:</h2>
         <table className="table table-bordered table-striped">
           <thead className="thead-dark">
             <tr>
