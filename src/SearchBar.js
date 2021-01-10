afc6cd12e6b38a452ade771b6b5523310f171030
Tristan <tristan15thompson@gmail.com>
2021-01-10 00:24:30 -0800

    [ADD] Search bar autofill feature

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -2,16 +2,46 @@ import React from 'react';
 import './SearchBar.css';
 import { Link, withRouter } from 'react-router-dom';
 import icon from './img/search-icon.png';
+import Autosuggest from 'react-autosuggest'
+
+let item_list = {}
+
+const getSuggestions = ((value) => {
+  console.log(value)
+  if(!value) return []
+  const inputValue = value.trim().toLowerCase();
+  const inputLength = inputValue.length;
+
+  return inputLength === 0 ? [] : item_list.filter(item =>
+    item.name.toLowerCase().slice(0, inputLength) === inputValue
+  ).slice(0, 10);
+});
+
+const getSuggestionValue = ((suggestion) => suggestion.name)
+
+const renderSuggestion = ((suggestion) => (
+  /**
+    * @param {string} suggestion
+    */
+  <div>{suggestion.name}</div>
+))
+
 
 class SearchBar extends React.Component {
   constructor() {
     super();
     this.state = {
-      query: "",
+      value: "",
       enterPressed: false,
+      suggestions: [],
     };
+
+    this.onChange = (event, {newValue}) => {
+      this.setState({ value: newValue });
+    }
     this.updateQuery = this.updateQuery.bind(this);
     this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.retrieveAllItems()
   }
 
   componentDidMount() {
@@ -22,8 +52,13 @@ class SearchBar extends React.Component {
     * @param {dict, dict} props
     */
     if(prevState.enterPressed !== this.state.enterPressed) {
-      this.setState({enterPressed: false})
+      this.setState({ enterPressed: false })
     }
+  }
+  async retrieveAllItems() {
+    const result = await fetch('/api/items');
+    const json = await result.json();
+    item_list = json
   }
 
   handleKeyDown(event) {
@@ -36,28 +71,57 @@ class SearchBar extends React.Component {
     }
   }
 
-  updateQuery(event) {
+  updateQuery(event, {newValue}) {
     /**
-    * @param {event} event
-    */
-    this.setState({ query: event.target.value });
+     * @param {event} event
+     */
+    this.setState({ query: newValue });
   }
+
+  onSuggestionsFetchRequested = (({value}) => {
+    /**
+     * @param {string} value
+     */
+    this.setState({ suggestions : getSuggestions(value) })
+  })
+
+  onSuggestionsClearRequested = (() =>{
+    /**
+     * This function clears the suggestions when we click out of the search bar
+     */
+    this.setState({ suggestions : [] })
+  })
 
   render() {
     /*This will give me something to avoid dom warnings*/
     const url = window.location.href;
     const newQuery = url.substring(30);
+    const { value, enterPressed, suggestions } = this.state;
 
-    if(this.state.enterPressed && this.state.query !== newQuery && this.state.query.trim() !== "") {
-      this.props.history.push("/items/" + this.state.query);
+    if(enterPressed && value && value !== newQuery && value.trim() !== "") {
+      this.props.history.push("/items/" + value);
+    }
+
+    const inputProps = {
+      placeholder: "Search and Item",
+      value,
+      onChange: this.onChange
     }
     return(
       <div>
         <div className="jumbotron form-group">
-          <input type="text"  className="form-control" value={this.state.query} onChange={this.updateQuery} />
-          <Link to={"/items/" + this.state.query} ><img src={icon} height="30" width="30" alt="Search Button"></img></Link>
+          <Autosuggest
+            suggestions={suggestions}
+            onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+            onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+            getSuggestionValue={getSuggestionValue}
+            renderSuggestion={renderSuggestion}
+            inputProps={inputProps}
+          />  
+          <Link to={"/items/" + value} ><img src={icon} height="30" width="30" alt="Search Button"></img></Link>
         </div>
       </div>
+
     );
   }
 }
