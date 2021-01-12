aded0a7c5e65e2b4135f45cecf2a81143752d106
Tristan <tristan15thompson@gmail.com>
2021-01-11 17:33:05 -0800

    [EDIT] cleaned code added docstrings

--- api/api.py
+++ api/api.py
@@ -1,8 +1,10 @@
-""" This file is the backend for our project. It will serve json data to our
-    front end, detailing information and statistics on each items. This data
-    will be stored in a mongoDB database, and will be retrieved as necessary
-    from the api provided by https://warframe.market/ via
-    http://api.warframe.market/v1/ """
+""" 
+This file is the backend for our project. It will serve json data to our
+front end, detailing information and statistics on each items. This data
+will be stored in a mongoDB database, and will be retrieved as necessary
+from the api provided by https://warframe.market/ via
+http://api.warframe.market/v1/ 
+"""
 
 from flask import Flask
 from flask_pymongo import PyMongo
@@ -20,48 +22,48 @@ host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/itemDatabase') +
 app.config["MONGO_URI"] = host
 mongo = PyMongo(app)
 
-# Route to start tracking an item for updates
 @app.route('/api/items/track/<item_id>')
 def track_item(item_id):
+  '''Route to start tracking an item for updates'''
     result = mongo.db.items.update_one({'item_id': item_id}, {"$set": {'needs_stats': True}})
     success = result.matched_count > 0
     return {"success": success}
 
-# Route to stop tracking an item for updates
 @app.route('/api/items/untrack/<item_id>')
 def untrack_item(item_id):
+  '''Route to stop tracking an item for updates'''
     result = mongo.db.items.update_one({'item_id': item_id}, {"$set": {'needs_stats': False}})
     success = result.matched_count > 0
     return {"success": success}
 
-# Route to provide information on a single item
 @app.route('/api/items/<item_id>')
 def request_item(item_id):
+  '''Route to provide information on a single item '''
     item = dumps(mongo.db.items.find({'item_id': item_id}))
     return item
 
-# Route to add an item to our watchlist
 @app.route('/api/watchlist/add/<item_id>')
 def add_to_watchlist(item_id):
+  '''Route to add an item to our watchlist'''
     result = mongo.db.items.update_one({'item_id': item_id}, {"$set": {'is_watched': True}})
     success = result.matched_count > 0
     return {"success": success}
 
-# Route to remove an item from our watchlist
 @app.route('/api/watchlist/remove/<item_id>')
 def remove_from_watchlist(item_id):
+  '''Route to remove an item from our watchlist'''
     result = mongo.db.items.update_one({'item_id': item_id}, {"$set": {'is_watched': False}})
     success = result.matched_count > 0
     return {"success": success}
 
-# Route to deliver all watched items
 @app.route('/api/watchlist/list')
 def retrieve_watchlist():
+  '''Route to deliver all watched items'''
     items = dumps(mongo.db.items.find({'is_watched': True}))
     return items
 
-# Route to deliver all items
 @app.route('/api/items')
 def retrieve_all_items():
+  '''Route to deliver all items'''
   items = dumps(mongo.db.items.find({}))
   return items
--- api/websocket.py
+++ api/websocket.py
@@ -10,58 +10,49 @@ db = client.itemDatabase
 async def storeResponse(message):
   new_order = {}
   responseJson = json.loads(message)
-  # Here I filter my websocket data to only show me new sell orders
+  # Filter websocket data on sell orders only
   if (responseJson["type"] == "@WS/SUBSCRIPTIONS/MOST_RECENT/NEW_ORDER" and responseJson["payload"]["order"]["order_type"] == "sell"):
-    print(json.dumps(responseJson, indent=2))
     item_id = responseJson["payload"]["order"]["item"]["url_name"]
-    # find an item and its order history
     db_item = db.items.find_one({'item_id': item_id})
     order_history = db_item["order_history"]
-    # only store 50 most recent orders
     if (len(order_history) > 50):
       order_history.pop()
-    # filter mods only, NOT RIVENS OR SYNDICATE ITEMS
+    # filter mods, not riven or syndicate mods
     if("mod" in responseJson["payload"]["order"]["item"]["tags"] and "riven" not in responseJson["payload"]["order"]["item"]["tags"] and "syndicate" not in responseJson["payload"]["order"]["item"]["tags"] and "parazon" not in responseJson["payload"]["order"]["item"]["tags"]):
-      #filter bad mods out
+      #filter out mods that are not at max rank
       if (responseJson["payload"]["order"]["mod_rank"] == responseJson["payload"]["order"]["item"]["mod_max_rank"]):
         new_order = {
         "platinum" : responseJson["payload"]["order"]["platinum"],
       }
       else:
-        return
+        return {"success": False}
     # everything else
     else:
       new_order = {
         "platinum" : responseJson["payload"]["order"]["platinum"],
       }
-    # Calculate min and max prices
+    order_history.append(new_order)
+
+    # Calculate min, max and average prices
     min_price = db_item["min_price"]
     max_price = db_item["max_price"]
     if responseJson["payload"]["order"]["platinum"] < min_price:
       min_price = responseJson["payload"]["order"]["platinum"]
     if responseJson["payload"]["order"]["platinum"] > max_price:
       max_price = responseJson["payload"]["order"]["platinum"]
-
-    #Calulate avg price
-    order_history.append(new_order)
-    print(order_history, "ORDER HISTORY---------------------")
     num_items = 0
     total_platinum = 0
     for item in order_history:
       total_platinum += item["platinum"]
       num_items += 1
-    avg_price = total_platinum / num_items
-
-    is_urgent = round((min_price < avg_price), 2)
+    avg_price = round(total_platinum / num_items, 2)
+    is_urgent = min_price < avg_price
 
     db.items.update_one({'item_id': item_id}, {"$set": {"order_history": order_history, "avg_price": avg_price, "min_price": min_price, "max_price": max_price, "is_urgent": is_urgent}})
-
-
-
-  print(json.dumps(message, indent = 2))
-  return "hello"
+  return {"success" : True}
 
 async def socket():
+  '''Websocket that connects to the Warframe Market Websocket'''
     async with websockets.connect(
             'wss://warframe.market/socket',
             timeout=30,
