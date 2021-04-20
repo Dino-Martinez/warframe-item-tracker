21e491c1800c765b45cb7397859d08dbae9f4805
Tristan Thompson <tristan15thompson@gmail.com>
2021-04-19 20:04:10 -0700

    [ADD] connected local mongoose database

---
+++ api/data/warframe-db.js
@@ -0,0 +1,29 @@
+/* Mongoose Connection */
+const mongoose = require("mongoose");
+const assert = require("assert");
+
+const url = "mongodb://localhost/warframe-db";
+
+mongoose.Promise = global.Promise;
+mongoose.connect(
+  url,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  },
+  function (err, db) {
+    assert.equal(null, err);
+    console.log("Connected successfully to database");
+
+    // db.close(); turn on for testing
+  }
+);
+mongoose.connection.on(
+  "error",
+  console.error.bind(console, "MongoDB connection Error:")
+);
+mongoose.set("debug", true);
+
+module.exports = mongoose.connection;
