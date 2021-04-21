3b3d1536d70195a178fe27038240d799306b2233
Tristan Thompson <tristan15thompson@gmail.com>
2021-04-21 15:08:22 -0700

    [ADD] more controller routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,12 @@
 const express = require("express")
 const usersRoutes = require("./users.js")
+const apiRoutes = require("./api.js")
+const socketsRoutes = require("./sockets.js")
 
 const router = express.Router()
 
 router.use("/users", usersRoutes)
+router.use("/api", apiRoutes)
+router.use("/socket", socketsRoutes)
 
 module.exports = router
