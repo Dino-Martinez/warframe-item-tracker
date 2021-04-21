2e739d10e9f5f532981ee58c00f29619666db1b9
Dino-Martinez <Dino.Martinez.514@gmail.com>
2021-04-21 15:38:45 -0700

    [Update] Integrated wf.m websocket in our express server

--- api/server.js
+++ api/server.js
@@ -1,13 +1,26 @@
-require('dotenv/config')
-const express = require('express')
+require("dotenv/config");
+const express = require("express");
 const bodyParser = require("body-parser");
+const WebSocket = require("ws");
 
+// Create websocket to receive new item postings from wf.m
+const ws = new WebSocket("wss://warframe.market/socket");
+ws.on("open", () => {
+  // Send wf.m our request for socket data
+  console.log("Opened connection");
+  ws.send('{"type": "@WS/SUBSCRIBE/MOST_RECENT"}');
+});
+
+// Whenever we receive a new item posting, store it in our database
+ws.on("message", data => {
+  console.log(data);
+});
 
 // Set db
 require("./data/warframe-db");
 
 // Configure app
-const app = express()
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,12 +29,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 const router = require("./routes/index.js");
 app.use(router);
 
-app.get('/', (req, res) => {
-  res.send({"Hello": "World"})
-})
+app.get("/", (req, res) => {
+  res.send({ Hello: "World" });
+});
 
 app.listen(process.env.PORT, () => {
-  console.log(`Listening on port ${process.env.PORT}!`)
-})
+  console.log(`Listening on port ${process.env.PORT}!`);
+});
 
-module.exports = app
+module.exports = app;
