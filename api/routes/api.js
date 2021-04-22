03037d1300aade180794bfdf753e849d87d6a860
Tristan Thompson <tristan15thompson@gmail.com>
2021-04-21 17:59:41 -0700

    [FIX] broken route name

--- api/routes/api.js
+++ api/routes/api.js
@@ -5,7 +5,7 @@ const Item = require("../models/item")
 
 router.post("/items/track/:item_id", (req, res) => {
   /** Route to start tracking an item for updates */
-  Item.findById(req.params.id).exec(function (err, item) {
+  Item.findById(req.params.item_id).exec(function (err, item) {
     item.needs_stats = true
     item.save()
 
@@ -15,7 +15,7 @@ router.post("/items/track/:item_id", (req, res) => {
 
 router.post("/items/untrack/:item_id", (req, res) => {
   /** Route to stop tracking an item for updates */
-  Item.findById(req.params.id).exec(function (err, item) {
+  Item.findById(req.params.item_id).exec(function (err, item) {
     item.needs_stats = false
     item.save()
 
@@ -23,16 +23,24 @@ router.post("/items/untrack/:item_id", (req, res) => {
   })
 })
 
-router.get("/items/test", (req, res) => {
+router.get("/items/:item_id", (req, res) => {
+  /** Route to provide information on a single item */
+  Item.findById(req.params.item_id).exec(function (err, item) {
+    res.send(item)
+  })
+})
+
+router.get("/items_tests", (req, res) => {
   /**test route for adding an item*/
   const item = new Item()
   item.item_id = "test_id"
   item.name = "test_name"
-  item.needs_stats =
+  item.needs_stats = false
 
   item
     .save()
     .then((item) => {
+      console.log("slkdjfhlas")
       res.send({"status": 200})
     })
 })
